875f30590387e56b2f8774db1550f23099a31302
Wahid Rizqi Akbar Nurjaman <v-wahid.rakbar@bukittechnology.com>
2023-10-14 11:46:44 +0700

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-center text-3xl text-blue-500">Hello</h1>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="bg-teal-700 h-full">
+    <p>QR Generator</p>
+    <p>QR Scanner</p>
+  </main>
+</template>
